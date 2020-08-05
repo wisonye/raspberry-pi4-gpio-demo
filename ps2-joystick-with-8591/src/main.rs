3a16182590f3ef9d82db1bdb4e5df833c71fd757
Wison Ye <wisonye@gmail.com>
2020-08-05 07:37:48 +0000

    [ master ] - Added 'ps-joystick-with-8591'.

---
+++ ps2-joystick-with-8591/src/main.rs
@@ -0,0 +1,52 @@
+/// Use PCF8591 (AD/DA converter) to control PS joystick
+///
+/// How to connect the circuit: 
+///
+/// Pi               8591              Joystick       Pi
+/// ====             ====              ========       ====
+/// 3.3V ----------  VCC               +5V      ----- 3.3V
+/// GND  ----------  GND               GND      ----- GND
+/// SDA  ----------  SDA
+/// SCL  ----------  SCL
+///                  AIN0  ----------- VRX
+///                  AIN1  ----------- VRY
+///                  AIN2  ----------- SW
+///
+/// You need to run by `sudo` if you see the error below:
+///
+/// `Error: PermissionDenied("/dev/gpiomem")`
+///
+
+use std::{time::Duration, thread};
+use rppal::gpio::{Result};
+use pcf8591::{PCF8591, Pin};
+// use gpio_util::{GpioUtil, PinState};
+
+fn main() -> Result<()> {
+    let mut converter = PCF8591::new("/dev/i2c-1", 0x48, 3.3).unwrap();
+
+    loop {
+        // Analog value is a byte (0~255)
+        let x = converter.analog_read_byte(Pin::AIN0).unwrap();
+        let y = converter.analog_read_byte(Pin::AIN1).unwrap();
+        let z = converter.analog_read_byte(Pin::AIN2).unwrap();
+        // println!("x: {}", x);
+        // println!("y: {}", y);
+        // println!("z: {}", z);
+        //
+
+        let mut status: String = String::new();
+
+        if x == 0 { status.push_str("Left,"); }
+        else if (x == 255) { status.push_str("Right,"); }
+
+        if y == 0 { status.push_str("Up,"); }
+        else if (y == 255) { status.push_str("Down,"); }
+
+        if z == 0 { status.push_str("Button pressed"); }
+
+        if status.len() > 0 { println!("Joystick status: {}", status);}
+
+        thread::sleep(Duration::from_millis(10));
+    }
+}
